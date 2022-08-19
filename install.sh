8931d383a64a9d7d97990baf35f896b6f4bad9e6
Kamachi <68025706+hugo-berendi@users.noreply.github.com>
2022-08-19 21:11:19 +0200

    Add files via upload

--- install.sh
+++ install.sh
@@ -2,10 +2,10 @@
 
 case "$OSTYPE" in
         solaris*) echo "SOLARIS" ;;
-        darwin*)  echo "OSX" ;; 
-        linux*)   echo "LINUX" ;;
+        darwin*)  curl -O https://raw.githubusercontent.com/hugo-berendi/setup/main/macos.sh && bash macos.sh ;; 
+        linux*)   curl -O https://raw.githubusercontent.com/hugo-berendi/setup/main/windows.sh && bash windows.sh ;;
         bsd*)     echo "BSD" ;;
-        msys*)    curl -O https://raw.githubusercontent.com/hugo-berendi/dotfiles/main/install-dotfiles.sh && bash install-dotfiles.sh ;;
+        msys*)    curl -O https://raw.githubusercontent.com/hugo-berendi/setup/main/linux.sh && bash linux.sh ;;
         cygwin*)  echo "ALSO WINDOWS" ;;
         *)        echo "unknown: $OSTYPE" ;;
 esac
