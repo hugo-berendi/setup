8ba90466507f30a0bb1f74b78dfe7cd720f71e21
hugo-berendi <hugo.berendi@outlook.de>
2022-08-20 19:52:42 +0200

    .

--- install.sh
+++ install.sh
@@ -3,10 +3,10 @@
 # find the os and install the installer
 
 case "$OSTYPE" in
-        solaris*) curl --silent -o- https://raw.githubusercontent.com/hugo-berendi/setup/main/linux.sh | bash ;;
+        solaris*) curl --silent -o- https://raw.githubusercontent.com/hugo-berendi/setup/main/linux/linux.sh | bash ;;
         darwin*)  curl --silent -o- https://raw.githubusercontent.com/hugo-berendi/setup/main/macos/macos.sh | bash ;; 
-        linux*)   curl --silent -o- https://raw.githubusercontent.com/hugo-berendi/setup/main/linux.sh | bash ;;
-        bsd*)     curl --silent -o- https://raw.githubusercontent.com/hugo-berendi/setup/main/linux.sh | bash ;;
+        linux*)   curl --silent -o- https://raw.githubusercontent.com/hugo-berendi/setup/main/linux/linux.sh | bash ;;
+        bsd*)     curl --silent -o- https://raw.githubusercontent.com/hugo-berendi/setup/main/linux/linux.sh | bash ;;
         msys*)    curl --silent -O https://raw.githubusercontent.com/hugo-berendi/setup/main/windows/windows.bat && ./windows.bat ;;
         cygwin*)  curl --silent -O https://raw.githubusercontent.com/hugo-berendi/setup/main/windows/windows.bat && ./windows.bat ;;
         *)        echo "unknown: $OSTYPE" ;;
