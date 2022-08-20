bd8643c47fc3527542797d60ea9a94f1026bf1cd
hugo-berendi <hugo.berendi@outlook.de>
2022-08-20 21:34:48 +0200

    .

--- install.sh
+++ install.sh
@@ -1,30 +1,24 @@
 #!/bin/bash
-if [ "$OSTYPE" == "linux-gnu" ]
-then
+if [ "$OSTYPE" == "linux-gnu" ]; then
         curl --silent -o- https://raw.githubusercontent.com/hugo-berendi/setup/main/linux/linux.sh | bash
 fi
 
-if [ "$OSTYPE" == "darwin"* ]
-then
+if [ "$OSTYPE" == "darwin"* ]; then
         curl --silent -o- https://raw.githubusercontent.com/hugo-berendi/setup/main/macos/macos.sh | bash
 fi
 
-if [ "$OSTYPE" == "cygwin" ]
-then
+if [ "$OSTYPE" == "cygwin" ]; then
         echo "There is Cygwin support yet"
 fi
 
-if [ "$OSTYPE" == "msys" ]
-then
+if [ "$OSTYPE" == "msys" ]; then
         curl --silent -O https://raw.githubusercontent.com/hugo-berendi/setup/main/windows/windows.bat && ./windows.bat
 fi
 
-if [ "$OSTYPE" == "win32" ]
-then
+if [ "$OSTYPE" == "win32" ]; then
         curl --silent -O https://raw.githubusercontent.com/hugo-berendi/setup/main/windows/windows.bat && ./windows.bat
 fi
 
-if [ "$OSTYPE" == "freebsd"* ]
-then
+if [ "$OSTYPE" == "freebsd"* ]; then
         echo "There is no FreeBSD support yet."
 fi
