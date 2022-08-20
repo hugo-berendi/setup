35a962d06979cf27c545310bd37406d78a605a6b
hugo-berendi <hugo.berendi@outlook.de>
2022-08-20 16:56:51 +0200

    .

--- install.sh
+++ install.sh
@@ -7,14 +7,14 @@ case "$OSTYPE" in
         darwin*)  curl -O https://raw.githubusercontent.com/hugo-berendi/setup/main/macos/macos.sh | bash macos.sh ;; 
         linux*)   curl -O https://raw.githubusercontent.com/hugo-berendi/setup/main/linux.sh | bash linux.sh ;;
         bsd*)     curl -O https://raw.githubusercontent.com/hugo-berendi/setup/main/linux.sh | bash linux.sh ;;
-        msys*)    curl -O https://raw.githubusercontent.com/hugo-berendi/setup/main/windows.sh | bash windows.sh ;;
-        cygwin*)  curl -O https://raw.githubusercontent.com/hugo-berendi/setup/main/windows.sh | bash windows.sh ;;
+        msys*)    curl -O https://raw.githubusercontent.com/hugo-berendi/setup/main/windows/how.sh | bash how.sh ;;
+        cygwin*)  curl -O https://raw.githubusercontent.com/hugo-berendi/setup/main/windows/how.sh | bash how.sh ;;
         *)        echo "unknown: $OSTYPE" ;;
 esac
 
 # end of the file
 
 echo ""
-echo "############################################################################################################"
+echo "##############################################################################"
 echo ""
 echo "Done"
