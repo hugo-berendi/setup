09001ab1a44b04c2ebbe4b74b79aade9b6859177
hugo-berendi <hugo.berendi@outlook.de>
2022-08-20 16:50:18 +0200

    .

--- install.sh
+++ install.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
+# find the os and install the installer
+
 case "$OSTYPE" in
-        solaris*) echo "SOLARIS" ;;
-        darwin*)  curl -O https://raw.githubusercontent.com/hugo-berendi/setup/main/macos.sh && bash macos.sh ;; 
-        linux*)   curl -O https://raw.githubusercontent.com/hugo-berendi/setup/main/windows.sh && bash windows.sh ;;
-        bsd*)     echo "BSD" ;;
-        msys*)    curl -O https://raw.githubusercontent.com/hugo-berendi/setup/main/linux.sh && bash linux.sh ;;
-        cygwin*)  echo "ALSO WINDOWS" ;;
+        solaris*) curl -O https://raw.githubusercontent.com/hugo-berendi/setup/main/linux.sh | bash linux.sh ;;
+        darwin*)  curl -O https://raw.githubusercontent.com/hugo-berendi/setup/main/macos/macos.sh | bash macos.sh ;; 
+        linux*)   curl -O https://raw.githubusercontent.com/hugo-berendi/setup/main/linux.sh | bash linux.sh ;;
+        bsd*)     curl -O https://raw.githubusercontent.com/hugo-berendi/setup/main/linux.sh | bash linux.sh ;;
+        msys*)    curl -O https://raw.githubusercontent.com/hugo-berendi/setup/main/windows.sh | bash windows.sh ;;
+        cygwin*)  curl -O https://raw.githubusercontent.com/hugo-berendi/setup/main/windows.sh | bash windows.sh ;;
         *)        echo "unknown: $OSTYPE" ;;
 esac
 
-echo "end of the file"
+# end of the file
+
+echo ""
+echo "############################################################################################################"
+echo ""
+echo "Done"
---
+++ macos/macos.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# install xcode command line tools
+
+xcode-select --install
+
+# install brew
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# install homebrew formulae
+
+curl -O https://raw.githubusercontent.com/hugo-berendi/setup/main/macos/formulae.txt
+xargs brew install < formulae.txt
+
+# install homebrew casks
+
+curl -O https://raw.githubusercontent.com/hugo-berendi/setup/main/macos/casks.txt
+xargs brew install < casks.txt
+
+
+# configure git
+
+echo ""
+echo "############################################################################################################"
+echo ""
+echo "Configuring git"
+echo "Git username:"
+read git_username
+echo ""
+echo "Git email:"
+read git_email
+
+git config --global user.name $git_username
+
+git config --global user.email $git_email
+
+git config --global core.editor neovim
+
+# install node version manager
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/HEAD/install.sh | bash
+
+nvm install 18
+
+# global npm installs
+
+nvm use 18
+npm install -g lite-server gitignore license yarn ts-node typescript
+
+# create dev folder
+
+mkdir ~/development
