dd495b590761ae8b1ca909db524feb422b39fa30
hugo-berendi <hugo.berendi@outlook.de>
2022-08-20 21:49:17 +0200

    .

--- linux/linux.sh
+++ linux/linux.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
+# install brew
+
+sudo apt-get install build-essential procps curl file git
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# install homebrew formulae
+
+curl --silent -O https://raw.githubusercontent.com/hugo-berendi/setup/main/linux/formulae.txt
+xargs brew install < formulae.txt
+
+# end of the file
+
 echo "Linux"
