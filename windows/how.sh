20ce5ac9f9e9d6e1492bb77251a92753ae407100
hugo-berendi <hugo.berendi@outlook.de>
2022-08-20 16:59:18 +0200

    .

--- windows/how.sh
+++ windows/how.sh
@@ -4,6 +4,10 @@ echo "2. Install via bash script"
 echo "Enter your choice [1/2]: "
 read install_method
 
+echo ""
+echo "##############################################################################"
+echo ""
+
 case "$install_method" in
         1*) echo "batch" ;;
         2*) echo "bash script" ;;
