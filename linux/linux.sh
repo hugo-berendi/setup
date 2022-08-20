b420f68d04b3f3a20f8919232378ab378e0b7b6e
hugo-berendi <hugo.berendi@outlook.de>
2022-08-20 23:33:22 +0200

    .

--- linux/linux.sh
+++ linux/linux.sh
@@ -4,6 +4,8 @@
 
 sudo apt-get install build-essential
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> $HOME/.bash_profile
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
 # install homebrew formulae
 
@@ -25,21 +27,23 @@ brew install peco
 
 # install fisher and set it up
 
-curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
-
+curl -sL -O https://git.io/fisher | source fisher.fish && fisher install jorgebucaran/fisher
 fisher install jethrokuan/z
 
 # install nerd font
 
 mkdir -p ~/.local/share/fonts
 cd ~/.local/share/fonts && curl -fLo "Hack Regular Nerd Font Complete Mono.ttf" https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/Hack/Regular/complete/Hack%20Regular%20Nerd%20Font%20Complete%20Mono.ttf
-
+cd ~
 
 # setting up dotfiles
 
+cd ~/.config/
+rm -r fish
+svn checkout https://github.com/hugo-berendi/dotfiles/trunk/config/fish
+svn checkout https://github.com/hugo-berendi/dotfiles/trunk/config/nvim
+svn checkout https://github.com/hugo-berendi/dotfiles/trunk/config/tmux
 cd ~
-svn checkout https://github.com/hugo-berendi/dotfiles/trunk/config
-mv config .config
 
 # end of the file
 
