699ae806cc378931a4c0eeeef39a168894dacc5b
hugo-berendi <hugo.berendi@outlook.de>
2022-08-20 21:16:55 +0200

    .

--- install.sh
+++ install.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
-
-# find the os and install the installer
-
-case "$OSTYPE" in
-        solaris*) curl --silent -o- https://raw.githubusercontent.com/hugo-berendi/setup/main/linux/linux.sh | bash ;;
-        darwin*)  curl --silent -o- https://raw.githubusercontent.com/hugo-berendi/setup/main/macos/macos.sh | bash ;; 
-        linux*)   curl --silent -o- https://raw.githubusercontent.com/hugo-berendi/setup/main/linux/linux.sh | bash ;;
-        bsd*)     curl --silent -o- https://raw.githubusercontent.com/hugo-berendi/setup/main/linux/linux.sh | bash ;;
-        msys*)    curl --silent -O https://raw.githubusercontent.com/hugo-berendi/setup/main/windows/windows.bat && ./windows.bat ;;
-        cygwin*)  curl --silent -O https://raw.githubusercontent.com/hugo-berendi/setup/main/windows/windows.bat && ./windows.bat ;;
-        *)        echo "unknown: $OSTYPE" ;;
-esac
+# read $OSTYPE with an if statement
+if [ "$OSTYPE" == "linux-gnu" ]; then
+    curl --silent -o- https://raw.githubusercontent.com/hugo-berendi/setup/main/linux/linux.sh | bash
+elif [ "$OSTYPE" == "darwin"* ]; then
+    curl --silent -o- https://raw.githubusercontent.com/hugo-berendi/setup/main/macos/macos.sh | bash
+elif [ "$OSTYPE" == "cygwin" ]; then
+    echo "There is Cygwin support yet"
+elif [ "$OSTYPE" == "msys" ]; then
+    curl --silent -O https://raw.githubusercontent.com/hugo-berendi/setup/main/windows/windows.bat && ./windows.bat
+elif [ "$OSTYPE" == "win32" ]; then
+    curl --silent -O https://raw.githubusercontent.com/hugo-berendi/setup/main/windows/windows.bat && ./windows.bat
+elif [ "$OSTYPE" == "freebsd"* ]; then
+    echo "There is no FreeBSD support yet."
+else
+    echo "Unknown"
+fi
