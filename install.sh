d331c0a2ca1fcb1e1ee54426423021c9ac2a5516
hugo-berendi <hugo.berendi@outlook.de>
2022-08-20 17:24:28 +0200

    .

--- install.sh
+++ install.sh
@@ -10,12 +10,4 @@ case "$OSTYPE" in
         msys*)    curl --silent -O https://raw.githubusercontent.com/hugo-berendi/setup/main/windows/windows.bat && ./windows.bat ;;
         cygwin*)  curl --silent -O https://raw.githubusercontent.com/hugo-berendi/setup/main/windows/windows.bat && ./windows.bat ;;
         *)        echo "unknown: $OSTYPE" ;;
-<<<<<<< HEAD
 esac
-
-# end of the file
-
-echo ""
-echo "##############################################################################"
-echo ""
-echo "Done"
